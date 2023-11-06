d72bbf65bfc0718e6476f6b71b88805483b74f53
PINKTAIGAH <giorgio.b.campanella@gmail.com>
2023-11-06 22:25:16 +0000

    Added inheretance for pdf classes

--- week-8/eventGenerator.py
+++ week-8/eventGenerator.py
@@ -4,12 +4,12 @@ import random
 from utils import find_max
 from scipy.integrate import quad
 
-class Linear(object):
+class ProbabilityDensityFunction(object):
     """
-    Class that will generate a random value according to a linear distribution using a box method
+    Parent class containing common methods and members to be used by all pdf classes
     """
 
-    def __init__(self, slope, intercept, bounds):
+    def __init__(self, bounds):
 
         if (not isinstance(bounds, tuple)):
             raise TypeError("Variable bound must be a tuple with the form (boundMin, boundMax)")
@@ -19,24 +19,13 @@ class Linear(object):
             raise ValueError("First element in tuple must be smaller than second")
         
         # Initialise class variables
-        self.intercept = intercept
-        self.slope = slope
         self.boundMin, self.boundMax = bounds
         # Initialise list to hold randomly generated mass values
         self.mass = []
-        # Find maximum value of the distribution within the bounds
-        self.maxValue = find_max(self._evaluate, self.boundMin, self.boundMax)
-
-    def _evaluate(self, x):
-        """
-        Evaluate the linear function of the distribution
-        NOTE: Returns un-normalised values
-        """
-        return self.intercept + self.slope * x
 
     def integrate(self, limits):
         """
-        Evaluate the integral of the linear function within the specified bounds
+        Evaluate the integral of the pdf within the specified bounds
         ##### NOTE: Integral is not normalised within the specified bounds of the class #####
         """
 
@@ -55,6 +44,36 @@ class Linear(object):
         integralResult, IntegralError = quad(self._evaluate, limitLow, limitHigh) 
         return integralResult
     
+    def getMass(self,):
+        """
+        Return numpy array containing all generated values
+        """
+
+        return np.array(self.mass)
+
+class Linear(ProbabilityDensityFunction):
+    """
+    Class that will generate a random value according to a linear distribution using a box method
+    """
+
+    def __init__(self, slope, intercept, bounds):
+
+        # Initialise parent class
+        super().__init__(bounds)
+
+        # Initialise class variables
+        self.intercept = intercept
+        self.slope = slope
+        # Find maximum value of the distribution within the bounds
+        self.maxValue = find_max(self._evaluate, self.boundMin, self.boundMax)
+
+    def _evaluate(self, x):
+        """
+        Evaluate the linear function of the distribution
+        NOTE: Returns un-normalised values
+        """
+        return self.intercept + self.slope * x
+
     def next(self,):
         """
         Generate a single random variable according to the class' distribution using the box method.
@@ -74,33 +93,19 @@ class Linear(object):
                 self.mass.append(filteredX)
                 return filteredX
 
-    def getMass(self,):
-        """
-        Return numpy array containing all generated values
-        """
-
-        return np.array(self.mass)
-
-class Gaussian(object):
+class Gaussian(ProbabilityDensityFunction):
     """
     Class that will generate a random value according to a gaussian distribution using numpy.random.normal
     """
 
     def __init__(self, mean, sigma, bounds):
 
-        if (not isinstance(bounds, tuple)):
-            raise TypeError("Variable bound must be a tuple with the form (boundMin, boundMax)")
-        if (not len(bounds) == 2):
-            raise ValueError("Variable bound must have form (boundMin, boundMax)")
-        if (not bounds[0] < bounds[1]):
-            raise ValueError("First element in tuple must be smaller than second")
+        # Initialise parent class
+        super().__init__(bounds)
         
         # Initialise class variables
         self.mean = mean
         self.sigma = sigma
-        self.boundMin, self.boundMax = bounds
-        # Initialise list to hold randomly generated mass values
-        self.mass = []
         # Find maximum value of the distribution within the bounds
         self.maxValue = find_max(self._evaluate, self.boundMin, self.boundMax)
 
@@ -111,27 +116,6 @@ class Gaussian(object):
         """
         return 1/(self.sigma * np.sqrt(2.0*np.pi)) * np.exp( -(x-self.mean)**2 / (2.0 * self.sigma**2) )
 
-    def integrate(self, limits):
-        """
-        Evaluate the integral of the gaussian function within the specified bounds
-        ##### NOTE: Integral is not normalised within the specified bounds of the class #####
-        """
-
-        if (not isinstance(limits, tuple)):
-            raise TypeError("Variable bound must be a tuple with the form (limitMin, limitMax)")
-        if (not len(limits) == 2):
-            raise ValueError("Variable bound must have form (limitMin, limitMax)")
-        if (not limits[0] < limits[1]):
-            raise ValueError("First element in tuple must be smaller than second")
-        if (not limits[0] >= self.boundMin):
-            raise ValueError("Lower integral limit must be larger than lower bound of pdf")
-        if (not limits[1] <= self.boundMax):
-            raise ValueError("Higher integral limit must be smaller than upper bound of pdf")
-    
-        limitLow, limitHigh = limits
-        integralResult, IntegralError = quad(self._evaluate, limitLow, limitHigh) 
-        return integralResult
-    
     def next(self,):
         """
         Generate a single random variable according to the class' distribution using numpy.random.normal method
@@ -143,34 +127,20 @@ class Gaussian(object):
         self.mass.append(x)
         return x
 
-    def getMass(self,):
-        """
-        Return numpy array containing all generated values
-        """
-
-        return np.array(self.mass)
-
-class SignalWithBackground(object):
+class SignalWithBackground(ProbabilityDensityFunction):
     """
     Class that will generate a random distribution consisting of gaussian signal with a linear background signal
     """
 
     def __init__(self, mean, sigma, slope, intercept, bounds, signalFraction,):
         
-        if (not signalFraction > 0.0) and (not signalFraction < 1.0):
-            raise ValueError("Variable signal fraction must be a float within the bounds 0.0 <= signalFraction <= 1.0")
-        if not isinstance(bounds, tuple):
-            raise TypeError("Variable bound must be a tuple with the form (boundMin, boundMax)")
-        if not len(bounds) == 2:
-            raise ValueError("Variable bound must have form (boundMin, boundMax)")
-        if (not bounds[0] < bounds[1]):
-            raise ValueError("First element in tuple must be smaller than second")
+        # Initialise parent class
+        super().__init__(bounds)
 
         self.signalFraction = signalFraction
         # Initialise lists to hold randomly generated values
         self.massBackground = []
         self.massSignal = []
-        self.mass = []
     
         # Initialise pdf objects
         self.signal = Gaussian(mean, sigma, bounds)
@@ -182,27 +152,6 @@ class SignalWithBackground(object):
         NOTE: Returns un-normalised values between the bounds
         """
         return self.signalFraction*self.signal._evaluate(x) + (1-self.signalFraction)*self.background._evaluate(x)
-
-    def integrate(self, limits):
-        """
-        Evaluate the integral of the signal with background function within the specified bounds
-        ##### NOTE: Integral is not normalised within the specified bounds of the class #####
-        """
-
-        if (not isinstance(limits, tuple)):
-            raise TypeError("Variable bound must be a tuple with the form (limitMin, limitMax)")
-        if (not len(limits) == 2):
-            raise ValueError("Variable bound must have form (limitMin, limitMax)")
-        if (not limits[0] < limits[1]):
-            raise ValueError("First element in tuple must be smaller than second")
-        if (not limits[0] >= self.boundMin):
-            raise ValueError("Lower integral limit must be larger than lower bound of pdf")
-        if (not limits[1] <= self.boundMax):
-            raise ValueError("Higher integral limit must be smaller than upper bound of pdf")
-    
-        limitLow, limitHigh = limits
-        integralResult, IntegralError = quad(self._evaluate, limitLow, limitHigh) 
-        return integralResult
 
     def next(self,):
         """
@@ -224,13 +173,6 @@ class SignalWithBackground(object):
         # Add filtered x to mass list irrespective of what distribution it was drawn from
         self.mass.append(filteredX)
         return filteredX
-
-    def getMass(self,):
-        """
-        Return numpy array containing all generated values for overall distribution
-        """
-
-        return np.array(self.mass)
 
     def getMassBackground(self,):
         """
