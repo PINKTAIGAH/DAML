00b11dd2dca027ed137c8fb15413998b75234014
PINKTAIGAH <giorgio.b.campanella@gmail.com>
2024-01-30 15:28:30 +0000

    Finished week 14

--- week-14/GeantExample1Part2/src/DetectorConstruction.cpp
+++ week-14/GeantExample1Part2/src/DetectorConstruction.cpp
@@ -5,6 +5,7 @@
 #include "G4NistManager.hh"
 #include "G4Box.hh"
 #include "G4Tubs.hh"
+#include "G4EllipticalTube.hh"
 #include "G4LogicalVolume.hh"
 #include "G4PVPlacement.hh"
 #include "G4AutoDelete.hh"
@@ -31,11 +32,16 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
   G4NistManager* nistManager = G4NistManager::Instance();
   G4Material* air = nistManager->FindOrBuildMaterial( "G4_AIR" );
   G4Material* lead = nistManager->FindOrBuildMaterial( "G4_Pb" );
+  G4Material* liquidArgon = nistManager->FindOrBuildMaterial( "G4_lAr" );
+  G4Material* plutonium_dioxide = nistManager->FindOrBuildMaterial( "G4_PLUTONIUM_DIOXIDE" );
 
   // Sizes of the principal geometrical components (solids)
   G4double shieldThickness = 2.0*cm;
   G4double shieldRadius = 50.0*cm;
   G4double worldLength = 250.0*cm;
+  G4double targetXSemiAxis = 30.0*cm;
+  G4double targetYSemiAxis = 25.0*cm;
+  G4double targetHeight = 50*cm;
 
   // Definitions of Solids, Logical Volumes, Physical Volumes
 
@@ -92,8 +98,34 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
                  0,               // copy number
                  true );          // checking overlaps
 
+  // TARGET: Solid (Elliptical tube)
+  G4EllipticalTube* targetS = new G4EllipticalTube(
+                "Target",         // name
+                targetXSemiAxis,  // x axis dims
+                targetYSemiAxis,  // y axis dims
+                targetHeight );
+
+  // TARGET: Logical volume 
+  G4LogicalVolume* targetLV = new G4LogicalVolume(
+                 targetS,         // its solid
+                 liquidArgon,     // its material
+                 "Target",        // its name
+                 0, 0, 0 );       // Modifiers we don't use
+  
+  // TARGET: Physical volume
+  G4VPhysicalVolume* targetPV = new G4PVPlacement(
+                 0,               // no rotation
+                 G4ThreeVector(0.0*cm, 0.0*cm, 65.0*cm), // in the centre
+                 targetLV,        // its logical volume
+                 "Target",        // its name
+                 worldLV,         // its mother volume
+                 false,           // no boolean operations
+                 0,               // copy number
+                 true );          // checking overlaps
+
   // SHIELD: Warn if there's an overlap
   if ( shieldPV->CheckOverlaps() ) std::cerr << "WARNING: your simulated objects overlap" << std::endl;
+  if ( targetPV->CheckOverlaps() ) std::cerr << "WARNING: your simulated objects overlap" << std::endl;
 
   // Always return the physical world
   return worldPV;
@@ -116,4 +148,8 @@ void DetectorConstruction::ConstructSDandField()
   auto shieldDetector = new EnergyCounter( "Shield", 0 ); // Refers to histogram and ntuple ID 0
   G4SDManager::GetSDMpointer()->AddNewDetector( shieldDetector );
   this->SetSensitiveDetector( "Shield", shieldDetector );
+
+  auto targetDetector = new EnergyCounter( "Target", 1 ); // Refers to histogram and ntuple ID 1
+  G4SDManager::GetSDMpointer()->AddNewDetector( targetDetector );
+  this->SetSensitiveDetector( "Target", targetDetector );
 }
--- week-14/GeantExample1Part2/src/RunAction.cpp
+++ week-14/GeantExample1Part2/src/RunAction.cpp
@@ -10,17 +10,22 @@ RunAction::RunAction() : G4UserRunAction()
 
   // Add histograms to analysis (100 bins from 0 to 500 MeV)
   analysisManager->CreateH1( "Shield", "Shield deposited energy", 100, 0.0, 500.0*MeV ); // histogram id 0
+  analysisManager->CreateH1( "Target", "Target deposited energy", 100, 0.0, 500.0*MeV ); // histogram id 1
   // Next histogram will have id 1
 
   // Render the histograms in a .ps file
   analysisManager->SetH1Plotting( 0, true ); // set histogram id 0 to be plotted
+  analysisManager->SetH1Plotting( 1, true ); // set histogram id 1 to be plotted
 
   // Add an ntuple for Shield energy deposits
   analysisManager->CreateNtuple( "Shield", "Shield deposited energy" ); // ntuple id 0
   analysisManager->CreateNtupleDColumn( "Shield" ); // column id 0
   // Next column will have id 1
+  // Add an ntuple for Target energy deposits
+  analysisManager->CreateNtuple( "Target", "Target deposited energy" ); // ntuple id 1
+  analysisManager->CreateNtupleDColumn( "Target" ); // column id 1
   analysisManager->FinishNtuple();
-  // Next ntuple will have id 1
+  // Next ntuple will have id 2
 }
 
 RunAction::~RunAction()
